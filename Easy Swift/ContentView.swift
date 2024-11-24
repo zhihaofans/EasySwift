945575c6fcd918f0f3a7c146a22eed150f78ce4b
zhihaofans <zhihaofans@hotmail.com>
2024-11-24 22:12:30 +0800

    Initial Commit

---
+++ Easy Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Easy Swift
+//
+//  Created by zzh on 2024/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Easy Swift/Easy_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_SwiftApp.swift
+//  Easy Swift
+//
+//  Created by zzh on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Easy_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
