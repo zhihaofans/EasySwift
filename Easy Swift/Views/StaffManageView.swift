1adf148934cf3e1a6f190cfed62257971d3d1b67
zhihaofans <zhihaofans@hotmail.com>
2024-12-31 00:16:44 +0800

    add StaffManageView

---
+++ Easy Swift/Views/StaffManageView.swift
@@ -0,0 +1,21 @@
+//
+//  StaffManageView.swift
+//  Easy Swift
+//
+//  Created by zzh on 2024/12/31.
+//
+
+import SwiftUI
+import SwiftUtils
+
+struct StaffManageView: View {
+    var body: some View {
+        VStack {
+            
+        }.setNavigationTitle("员工管理")
+    }
+}
+
+#Preview {
+    StaffManageView()
+}
