1e52d948c97e396ab42c767e1976fc57196d53b8
zhihaofans <zhihaofans@hotmail.com>
2025-02-01 21:43:26 +0800

    更新依赖，优化代码

--- Easy Swift/Views/CalculatorView.swift
+++ Easy Swift/Views/CalculatorView.swift
@@ -13,10 +13,8 @@ struct CalculatorView: View {
     @State private var alertText: String="未知错误"
     var body: some View {
         VStack {
-            NavigationView {
-                List {
-                    NavigationLink("人体BMI计算", destination: BMICalculatorView())
-                }
+            List {
+                NavigationLink("人体BMI计算", destination: BMICalculatorView())
             }
         }
         .alert(self.alertTitle, isPresented: self.$showingAlert) {
@@ -49,14 +47,14 @@ private struct BMICalculatorView: View {
     var body: some View {
         VStack {
             List {
-                TextField("身高(m)", text: self.$inputHeight).setNumberType()
-                TextField("体重(kg)", text: self.$inputWeight)
+                TextField("身高(m)", text: self.$inputHeight).setDecimalType()
+                TextField("体重(kg)", text: self.$inputWeight).setDecimalType()
                 Button(action: {
                     if self.inputHeight.isNotEmpty {
                         let height=Float(inputHeight) ?? 1.5
                         let weight=Float(inputWeight) ?? 0.0
                         if self.inputHeight.isFloat, self.inputWeight.isFloat, height > 0, weight > 0 {
-                            let bmi=self.roundedDecimal(weight / (height * height), 1)
+                            let bmi=(weight / (height * height)).roundedDecimal(1)
                             var resultDec=""
                             if bmi < 18.5 {
                                 resultDec="过轻"
@@ -79,11 +77,9 @@ private struct BMICalculatorView: View {
                 }
                 Text(self.result)
             }
-        }.showTextAlert(self.alertTitle, self.alertMessage, isPresented: self.$isShowAlert)
-    }
-
-    func roundedDecimal(_ value: Float, _ decimalPlaces: Int=0) -> Float {
-        return (value * pow(10, Float(decimalPlaces))).rounded() / pow(10, Float(decimalPlaces))
+        }
+        .setNavigationTitle("BMI计算")
+        .showTextAlert(self.alertTitle, self.alertMessage, isPresented: self.$isShowAlert)
     }
 }
 
