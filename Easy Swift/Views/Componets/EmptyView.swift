0599d216c61b24cb42bb1f9509e1e8db47f228c0
zhihaofans <zhihaofans@hotmail.com>
2025-02-01 21:50:30 +0800

    新增EmptyTextView

---
+++ Easy Swift/Views/Componets/EmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyView.swift
+//  Easy Swift
+//
+//  Created by zzh on 2025/2/1.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyView()
+}
